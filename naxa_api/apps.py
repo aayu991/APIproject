775c57d1434880ab65a5d92b33f23e718bfdb7d6
aayu991 <aayushmishra315@gmail.com>
2023-03-19 21:09:32 +0545

    API

---
+++ naxa_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NaxaApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'naxa_api'
---
+++ naxatask/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from naxa_api import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('excel/', views.ReadExcelCreateApiView.as_view(), name='excel'),
+    path('excel/list', views.ReadExcelListApiView.as_view(), name='excel-list'),
+    path('excel/filter', views.ReadExcelFilterApiView.as_view(), name='excel-list'),
+    path('excel/municipality', views.ReadExcelMunicipalityApiView.as_view(), name='excel-list')
+]
